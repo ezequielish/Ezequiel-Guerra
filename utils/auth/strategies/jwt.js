67c0fd7aef80951c67aa260bfb121c1930e032d6
ezequiel <ezequielprophetic@gmail.com>
2019-10-18 16:32:07 -0400

    jwt

--- utils/auth/strategies/jwt.js
+++ utils/auth/strategies/jwt.js
@@ -1,30 +1,30 @@
-const passport = require('passport');
-const { ExtractJwt } = require('passport-jwt');
-const JwtStrategy = require('passport-jwt').Strategy
-const boom = require('@hapi/boom');
+// const passport = require('passport');
+// const { ExtractJwt } = require('passport-jwt');
+// const JwtStrategy = require('passport-jwt').Strategy
+// const boom = require('@hapi/boom');
 
-const { getUser } = require('../../../components/users/controller')
-const { authJwtSecret } = require('../../../config');
+// const { getUser } = require('../../../components/users/controller')
+// const { authJwtSecret } = require('../../../config');
 
-passport.use(
-  new JwtStrategy(
-    {
-      secretOrKey: authJwtSecret,
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
-    },
-    async function(tokenPayload, cb) {
+// passport.use(
+//   new JwtStrategy(
+//     {
+//       secretOrKey: authJwtSecret,
+//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+//     },
+//     async function(tokenPayload, cb) {
   
-      try {
-        const user = await getUser(tokenPayload.username);
-        if (!user) {
-          return cb(boom.unauthorized(), false);
-        }
+//       try {
+//         const user = await getUser(tokenPayload.username);
+//         if (!user) {
+//           return cb(boom.unauthorized(), false);
+//         }
 
-        delete user.password;
-        cb(null, { ...user, scopes: tokenPayload.scopes });
-      } catch (error) {
-        return cb(error);
-      }
-    }
-  )
-);
+//         delete user.password;
+//         cb(null, { ...user, scopes: tokenPayload.scopes });
+//       } catch (error) {
+//         return cb(error);
+//       }
+//     }
+//   )
+// );
